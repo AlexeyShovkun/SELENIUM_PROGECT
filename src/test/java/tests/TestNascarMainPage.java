fb5e5cad8cb9a558d42ffdbb35a30bd80b9aeba4
AlexeyShovkun <a.shovkun88@gmail.com>
2016-12-19 13:15:11 +0200

    Add files via upload

--- src/test/java/tests/TestNascarMainPage.java
+++ src/test/java/tests/TestNascarMainPage.java
@@ -7,21 +7,22 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import pages.MainPageNascar;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit; //CONCIRENT TIME UNIT
 
 /**
  * Created by Admin on 06.11.2016.
+ main class
  */
 public class TestNascarMainPage {
-    private WebDriver driver;
+    private WebDriver driver;       /*WEBDRIVER OBJECT*/
     private String URL = "http://www.nascar.com/";
 
     @BeforeSuite
     public void setup(){
-        System.setProperty("webdriver.chrome.driver", "D:\\ProjectsJava\\AutomationTest\\src\\test\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\DEV_PROJECTS\\java\\seltests\\NascarPageObjectExampleJava-master\\src\\test\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
-        driver.navigate().to(URL);
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        driver.navigate().to(URL); /*OPEN SITE*/
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
@@ -32,6 +33,20 @@ public class TestNascarMainPage {
         page.setPasswordIntoPasswordFieldInLoginForm("P@ssw0rd");
         page.submitLoginForm();
         Assert.assertTrue(page.errorIsShow());// AR and ER
+    }
+    @Test
+    public void  VerifyRegisterPage() {
+        MainPageNascar page1 = new MainPageNascar(driver);
+        page1.clickRegistrationButton();
+        page1.setFieldEmailInRegistrationForm("rikvizdikciya17@gmail.com"); /*Пока перед каждым запуском изменяем вручную*/
+        page1.setPasswordIntoPasswordFieldInRegisterForm("ad7672192");
+        page1.setZipCodeIntoZipCodeFieldInRegisterForm("08000");
+        page1.checkReceiveEmailConfirmationCheckBox();
+        page1.checPprivacyPolicyConfirmationCheckBox();
+        driver.navigate().to("http://www.nascar.com/en_us/ajax/dynamic/myNASCAR.html"); /*OPEN TWICE*/
+        page1.clickLogOutButtonOnMyProfilePage();
+        Assert.assertTrue(page1.errorIsShow());
+
     }
 
 
